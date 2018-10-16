f7c8cba64e34db0431bb3ca0d74e0c60531bc452
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-10-16 12:44:18 +0300

    Started week02

---
+++ week02/fact-iter.rkt
@@ -0,0 +1,26 @@
+#lang racket
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+(define (factorial-iter num)
+    (fact-iter 1 1 num))
+
+(require rackunit rackunit/text-ui)
+
+(define factorial-iter-tests
+  (test-suite
+   "Tests for factorial-iter"
+
+   (check = (factorial-iter 0) 1)
+   (check = (factorial-iter 1) 1)
+   (check = (factorial-iter 2) 2)
+   (check = (factorial-iter 3) 6)
+   (check = (factorial-iter 4) 24)
+   (check = (factorial-iter 5) 120)
+   (check = (factorial-iter 6) 720)
+   (check = (factorial-iter 7) 5040)))
+
+(run-tests factorial-iter-tests)
---
+++ week02/sum-iter.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (sum-iter start end)
+    (define (for start sum)
+    (if (> start end)
+        sum
+        (for (+ 1 start) (+ sum start))
+        )
+    )
+    (for start 0)
+)
+
+(require rackunit rackunit/text-ui)
+
+(define sum-iter-tests
+  (test-suite
+   "Tests for sum-iter"
+
+   (check = (sum-iter 1 1) 1)
+   (check = (sum-iter 1 2) 3)
+   (check = (sum-iter 1 3) 6)
+   (check = (sum-iter 0 4) 10)
+   (check = (sum-iter -4 2) -7)))
+
+(run-tests sum-iter-tests)
