e09fb3c4e8a707fb503284e5f31759e5023cc20a
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-10-17 11:53:38 +0300

    Added expt-iter

---
+++ week02/expt-iter.rkt
@@ -0,0 +1,64 @@
+#lang racket
+
+(define (expt-iter x n)
+    (define (for start res)
+      (if (< n 0)
+      (if (< start (+ n 1))
+          res
+       (for (- start 1) (* res (/ 1 x)))
+       )
+      (if (> start (- n 1))
+          res
+          (for (+ start 1) (* res x))
+          )
+      )
+      )
+    (for 0 1)
+)
+
+
+(require rackunit rackunit/text-ui)
+
+
+
+(define expt-iter-tests
+
+  (test-suite
+
+   "Tests for expt-iter"
+
+
+
+   (check = (expt-iter -2 -11) -1/2048)
+
+   (check = (expt-iter -2 -10) 1/1024)
+
+   (check = (expt-iter 2 -10) 1/1024)
+
+   (check = (expt-iter 5 -3) 1/125)
+
+   (check = (expt-iter 3 -2) 1/9)
+
+   (check = (expt-iter 2 -2) 1/4)
+
+   (check = (expt-iter 2 -1) 1/2)
+
+   (check = (expt-iter 2 0) 1)
+
+   (check = (expt-iter 2 1) 2)
+
+   (check = (expt-iter 2 2) 4)
+
+   (check = (expt-iter 3 2) 9)
+
+   (check = (expt-iter 5 3) 125)
+
+   (check = (expt-iter 2 10) 1024)
+
+   (check = (expt-iter -2 10) 1024)
+
+   (check = (expt-iter -2 11) -2048)))
+
+
+
+(run-tests expt-iter-tests)
