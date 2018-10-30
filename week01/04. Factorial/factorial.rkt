b87f3a8d2dec8bf616944c7691c42a0d9dc0a521
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2018-10-30 13:17:00 +0200

    Changed week01 structure

--- week01/04. Factorial/factorial.rkt
+++ week01/04. Factorial/factorial.rkt
@@ -2,5 +2,23 @@
 (define (factorial n)
     (cond
       [(< n 0) #f]
+      [(= n 0) 1]
       [(= n 1) n]
       (else ( * n (factorial (- n 1))))))
+
+(require rackunit rackunit/text-ui)
+
+(define factorial-tests
+  (test-suite
+   "Tests for factorial"
+
+   (check = (factorial 0) 1)
+   (check = (factorial 1) 1)
+   (check = (factorial 2) 2)
+   (check = (factorial 3) 6)
+   (check = (factorial 4) 24)
+   (check = (factorial 5) 120)
+   (check = (factorial 6) 720)
+   (check = (factorial 7) 5040)))
+
+(run-tests factorial-tests)
