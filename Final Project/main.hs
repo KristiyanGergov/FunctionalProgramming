6661c4c547e2fd49424caf3fb2117f0cb1c60731
Kristiyan Gergov <kristiqn1gergov@gmail.com>
2019-01-20 22:57:34 +0200

    Finished UI

--- Final Project/main.hs
+++ Final Project/main.hs
@@ -1,38 +1,31 @@
 data BloodType = Cold_Blooded | Warm_Blooded   
            deriving (Eq, Show, Read, Enum)  
 
-data AnimalType = Air | Sea | Earth | AirSea | AirEarth | SeaEarth | Mixed
-            deriving (Eq, Show, Read, Enum)
-            
-data Mammal = Yes | No
-            deriving (Eq, Show, Read, Enum)                
+data AnimalType = Fish | Invertebrate | Amphibian | Reptile | Bird | Mammal           
 
 allBloodTypes = [Cold_Blooded ..]
-allAnimalTypes = [Air ..]
-allMammals = [Yes ..]
-                                 
+allAnimalTypes = [Fish ..]
 
 data Animal = Animal {
   name :: String,
   bloodType :: BloodType,
   animalType :: AnimalType,
-  mammal :: Mammal
 } deriving (Eq, Show, Read)
 
 animals = [
-  Animal { name = "Cat", bloodType = Warm_Blooded, animalType = Earth, mammal = Yes},
-  Animal { name = "Dog", bloodType = Warm_Blooded, animalType = Earth, mammal = Yes},
-  Animal { name = "Pig", bloodType = Warm_Blooded, animalType = Earth, mammal = Yes},
-  Animal { name = "Pigeon", bloodType = Warm_Blooded, animalType = Air, mammal = No},
-  Animal { name = "Lizard", bloodType = Cold_Blooded, animalType = Mixed, mammal = No},
-  Animal { name = "Snake", bloodType = Cold_Blooded, animalType = Earth, mammal = No},
-  Animal { name = "Frog", bloodType = Cold_Blooded, animalType = SeaEarth, mammal = No},
-  Animal { name = "Spider", bloodType = Cold_Blooded, animalType = Earth, mammal = No},
-  Animal { name = "Scorpion", bloodType = Cold_Blooded, animalType = Earth, mammal = No},
-  Animal { name = "Fish", bloodType = Cold_Blooded, animalType = Sea, mammal = No},
-  Animal { name = "Elephant", bloodType = Warm_Blooded, animalType = Earth, mammal = Yes},
-  Animal { name = "Bear", bloodType = Warm_Blooded, animalType = SeaEarth, mammal = Yes},
-  Animal { name = "Rhino", bloodType = Warm_Blooded, animalType = Earth, mammal = Yes}
+  Animal { name = "Cat", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Dog", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Pig", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Pigeon", bloodType = Warm_Blooded, animalType = Bird },
+  Animal { name = "Lizard", bloodType = Cold_Blooded, animalType = Reptile },
+  Animal { name = "Snake", bloodType = Cold_Blooded, animalType = Reptile },
+  Animal { name = "Frog", bloodType = Cold_Blooded, animalType = Amphibian },
+  Animal { name = "Scorpion", bloodType = Cold_Blooded, animalType = Earth },
+  Animal { name = "Swordfish", bloodType = Cold_Blooded, animalType = Fish },
+  Animal { name = "Dolphin", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Elephant", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Bear", bloodType = Warm_Blooded, animalType = Mammal },
+  Animal { name = "Rhino", bloodType = Warm_Blooded, animalType = Mammal }
   ]
 
   
@@ -45,7 +38,7 @@ checkType currentAnimals currentTypes criteria "Yes" = do
 
 checkType currentAnimals currentTypes criteria "No" = do
   let headAnimalType = (head currentTypes)
-  let question = "Is it " ++ (show headAnimalType) ++ " animal? (Yes/No)"
+  let question = "Is it a/an" ++ (show headAnimalType) ++ " animal? (Yes/No)"
   putStrLn question
   do
     answer <- getLine
@@ -56,8 +49,11 @@ checkType currentAnimals currentTypes criteria "No" = do
       checkType currentAnimals (tail currentTypes) criteria answer      
       
 main = do 
-  filterByBloodType <- checkType animals allBloodTypes bloodType "No"
+  animalsFilteredByBloodType <- checkType animals allBloodTypes bloodType "No"
   
-  filterByAnimalType <- checkType animals allAnimalTypes animalType "No"
+  animalsFilteredByAnimalType <- checkType animalsFilteredByBloodType allAnimalTypes animalType "No"
 
-  putStrLn (show filterByAnimalType)
+  animalsFilteredByMammalType <- checkType animalsFilteredByAnimalType allMammals mammal "No"
+
+  let answer = "Is it " ++ (name (head animalsFilteredByMammalType)) ++ "?"
+  putStrLn answer
